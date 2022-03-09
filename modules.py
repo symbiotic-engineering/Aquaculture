ce619339cf5d566e5ac6c2fed8a6fe530decc91d
Rebecca McCabe <rmccabe@mit.edu>
2022-03-09 02:11:05 -0500

    type annotations assertions, return J g h, WEC dicts

--- modules.py
+++ modules.py
@@ -1,13 +1,16 @@
+import numpy as np
 from objects import *
+from typing import Tuple
 
-def sim(x,p):
+def sim(x: dict, p: dict) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     # merge input dicts
     ins = x | p
-    
+
     # create objects
-    wec = WEC(ins['capture_width'], ins['capture_width_ratio'], ins['wec_type'])
+    wec = WEC(ins['capture_width'], ins['capture_width_ratio_dict'],
+            ins['wave_damping_dict'], ins['wec_type'], ins['wec_unit_cost'])
     wave_in = Wave(ins['wave_height'], ins['wave_period'])
-    pen = Pen(ins['pen_diameter'], ins['pen_height'])
+    pen = Pen(ins['pen_diameter'], ins['pen_height'], ins['pen_unit_cost'])
 
     # run each module 
     wave_out = wave_climate(wec,wave_in)
@@ -18,30 +21,48 @@ def sim(x,p):
 
     # outputs
     cost_per_yield = price/fish_yield 
-    return pow, cost_per_yield, carrying_capacity
+    J = np.array(cost_per_yield)
+    g = np.array([pow, carrying_capacity])
+    h = np.array([])
+
+    return J, g, h
 
 
-def power(wec:WEC, Wave:Wave):
-    P_gen = Wave.power * wec.capture_width * wec.capture_width_ratio
+def power(wec: WEC, wave: Wave) -> float:
+    assert(isinstance(wec,WEC))
+    assert(isinstance(wave,Wave))
+
+    P_gen = wave.power * wec.capture_width * wec.capture_width_ratio
     return P_gen
 
-def econ(wec:WEC, pen:Pen):
+def econ(wec: WEC, pen: Pen) -> float:
+    assert(isinstance(wec,WEC))
+    assert(isinstance(pen,Pen))
+
     price = wec.price + pen.price
     return price
 
-def wave_climate(wec:WEC, wave:Wave):
+def wave_climate(wec: WEC, wave: Wave) -> Wave:
+    assert(isinstance(wec,WEC))
+    assert(isinstance(wave,Wave))
+
     Hs = wec.wave_damping * wave.Hs
     T = wave.T
     wave_out = Wave(Hs,T)
     return wave_out
 
-def fish(wave:Wave, pen:Pen):
+def fish(wave: Wave, pen: Pen) -> float:
+    assert(isinstance(wave,Wave))
+    assert(isinstance(pen,Pen))
+
     if wave.Hs < 1:
         fish_yield = 1
     else:
         fish_yield = 0.5
     return fish_yield
 
-def environment(pen:Pen):
+def environment(pen: Pen) -> float:
+    assert(isinstance(pen,Pen))
+    
     carrying_capacity = 1
     return carrying_capacity
--- objects.py
+++ objects.py
@@ -1,46 +1,53 @@
 from math import pi
+from typing import Dict
 
 class WEC:
-    def __init__(self, capture_width, capture_width_ratio, type):
+    def __init__(self, capture_width: float, 
+                capture_width_ratio_dict: Dict[str,float], 
+                wave_damping_dict: Dict[str,float], 
+                wec_type: str,
+                unit_cost: float) -> None:
+
         self.capture_width = capture_width
-        self.capture_width_ratio = capture_width_ratio
-        self.type = type
+        self.capture_width_ratio_dict = capture_width_ratio_dict
+        self.wave_damping_dict = wave_damping_dict
+        self.wec_type = wec_type
+        self.unit_cost = unit_cost
 
     @property
-    def price(self):
-        price = self.capture_width * 1000
+    def price(self) -> float:
+        price = self.capture_width * self.unit_cost
         return price
 
     @property
-    def wave_damping(self):
-        if self.type == 'attenuator':
-            damping = .5
-        elif self.type == 'point absorber':
-            damping = 1
-        elif self.type == 'terminator':
-            damping = .75
-        else: 
-            print('Error: Wrong WEC type')
+    def wave_damping(self) -> float:
+        damping = self.capture_width_ratio_dict[self.wec_type]
         return damping
 
+    @property
+    def capture_width_ratio(self) -> float:
+        capture_width_ratio = self.capture_width_ratio_dict[self.wec_type]
+        return capture_width_ratio
+
 class Wave:
-    def __init__(self,Hs,T):
+    def __init__(self, Hs: float, T: float) -> None:
         self.Hs = Hs
         self.T = T
         self.rho = 1000
         self.g = 9.81
     
     @property
-    def power(self):
+    def power(self) -> float:
         P_wave = 1/32 * 1/pi * self.rho * self.g**2 * self.Hs**2 * self.T
         return P_wave
 
 class Pen:
-    def __init__(self,D,H):
+    def __init__(self, D: float, H: float, unit_cost: float) -> None:
         self.D = D 
         self.H = H
+        self.unit_cost = unit_cost
 
     @property
-    def price(self):
-        price = self.D * self.H * 1000
+    def price(self) -> float:
+        price = self.D * self.H * self.unit_cost
         return price
