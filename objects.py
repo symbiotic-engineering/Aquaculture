cc71fa634179c72b067c94466f52bb39bae264d6
Rebecca McCabe <rmccabe@mit.edu>
2022-06-06 15:06:54 -0400

    python simulation at state of design review

--- objects.py
+++ objects.py
@@ -1,5 +1,7 @@
-from math import pi
+from math import cos, exp, pi
 from typing import Dict
+import numpy as np
+from scipy.integrate import trapz
 
 class WEC:
     def __init__(self, capture_width: float, 
@@ -42,12 +44,114 @@ class Wave:
         return P_wave
 
 class Pen:
-    def __init__(self, D: float, H: float, unit_cost: float) -> None:
+    def __init__(self, D: float, H: float, SD: float, n: float, spacing: float, 
+    unit_cost: float, loss_rate: float, harvest_weight: float, env_params: dict) -> None:
         self.D = D 
         self.H = H
+        self.SD = SD 
+        self.n = n 
+        self.spacing = spacing
+
         self.unit_cost = unit_cost
+        self.loss_rate = loss_rate
+        self.harvest_weight = harvest_weight
+
+        self.O2_in = env_params['O2_in']
+        self.O2_min = env_params['O2_min']
+        self.P_f = env_params['P_f']
+        self.P_p = env_params['P_p']
+        self.U_min = env_params['U_min']
+        self.tau = env_params['tau']
+        self.permeability = env_params['permeability']
+
+        self.F_f = env_params['F_f']
+        self.F_p = env_params['F_p']
+        self.F_c = env_params['F_c']
+
+        self.A_f = env_params['A_f']
+        self.A_p = env_params['A_p']
+        self.A_c = env_params['A_c']
+        
+        self.O_f = env_params['O_f']
+        self.O_p = env_params['O_p']
+        self.O_c = env_params['O_c']
+        
+        self.C_f = env_params['C_f']
+        self.C_p = env_params['C_p']
+        self.C_c = env_params['C_c']
+        
 
     @property
     def price(self) -> float:
         price = self.D * self.H * self.unit_cost
         return price
+
+    @property 
+    def volume(self) -> float:
+        volume = pi * self.D**2 / 4 * self.H
+        return volume
+
+    @property 
+    def DO2(self) -> float:
+        # specific energy content of feed
+        delta = self.F_f * self.C_f + self.F_p * self.C_p + self.F_c * self.C_c
+
+        # specific energy content of fish
+        C_f_star = 0.85 * self.C_p * self.P_p + self.C_f * self.P_f
+
+        # fraction of food energy contributed by progeins, fat, and carbs
+        E_p = self.F_p * self.C_p / delta
+        E_f = self.F_f * self.C_f / delta
+        E_c = self.F_c * self.C_c / delta
+
+        # metabolizable energy content of food
+        FL = (1-self.A_p) * E_p + (1-self.A_f) * E_f + (1-self.A_c) * E_c
+        BC = 0.3 * self.A_p * E_p + 0.05 * (self.A_f * E_f + self.A_c * E_c)
+        eps = 1 - FL - BC
+        eps_star = eps - 0.15 * self.F_p * self.C_p * self.A_p / delta
+
+        # Water temperature as a function of time
+        time = np.linspace(0,51,1) # time vector [weeks]
+        T = 52                  # period [weeks]
+        w = 2*pi/T              # frequency [1/weeks]
+        phi = 2*pi/3            # phase offset [-]
+        T_max = 23
+        T_min = 4
+        T_bar = (T_max+T_min)/2
+        T_amp = (T_max-T_min)/2
+        Temp = T_bar + T_amp * cos(w * time + phi)
+
+        # Fish growth as a function of time
+        a = 0.038
+        W_0 = 0
+        print(Temp)
+        #integral = trapz( exp(Temp*self.tau), x=time )
+        integral = Temp # fixme
+        W = (W_0**(1/3) + a/3 * integral)**3
+
+        # Growth rate as a function of time
+        b = 2/3
+        W_dot = a * W**b * exp(Temp * self.tau)
+
+        # Rate of energy ingested by fish, cal/day
+        alpha = 11
+        gamma = 0.8
+        Q_r = 1/eps_star * (alpha * W**gamma + a * C_f_star * W**b) * exp(time*self.tau)
+
+        # Respiratory oxygen demand with respect to protein, fat, and carb consumption of fish
+        DO2_p = (self.F_p * self.A_p * Q_r / delta - self.P_p * W_dot) * self.O_p
+        DO2_f = (self.F_f * self.A_f * Q_r / delta - self.P_f * W_dot) * self.O_f
+        DO2_c = self.F_c * self.A_c * Q_r / delta * self.O_c
+
+        # Total respiratory oxygen demand of fish per day
+        DO2 = DO2_p + DO2_f + DO2_c
+
+        return DO2
+
+    @property
+    def carrying_capacity(self) -> float:
+        length = self.n * self.D + self.spacing * (self.n-1)
+        carrying_capacity = (self.O2_in - self.O2_min) * length * self.H * self.permeability * self.U_min / self.DO2
+        print('carrying capacity: ', carrying_capacity)
+        return min(carrying_capacity)
+
