fb445febd062acf3e66bb1a622e70f14b9b7be1b
Gabriel <33742119+gabrielewig@users.noreply.github.com>
2023-02-28 18:15:55 -0500

    add flagging for conflicts

--- gis/gis_handler.py
+++ gis/gis_handler.py
@@ -7,16 +7,25 @@ precision = 5 # precision of input coordinates, 5dec~=1.1m
 class GISHandler:
     """A class to handle GIS raster data and optimizer points."""
         
-    def __init__(self, files):
+    def __init__(self, conditions, conflicts, scope):
         """Initializes handler by creating a GeoDataFrame to store point measurement data and a dictionary of loaded raster files."""
-        self.rasters = {}
-        self.points = gpd.GeoDataFrame(columns=['x', 'y', 'valid', 'result', 'geometry'], geometry='geometry')
+        self.conditions = {}
+        self.conflicts = {}
+        self.points = gpd.GeoDataFrame(columns=['x', 'y', 'ok-conditions', 'ok-scope', 'ok-conflicts', 'result', 'geometry'], geometry='geometry')
         
-        for key, src in files.items():
-            if key in self.rasters:
+        self.scope = gpd.read_file(scope)
+    
+        for key, src in conditions.items():
+            if key in self.conditions:
                 print('raster {} already loaded!'.format(key))
             else:
-                self.rasters[key] = rasterio.open(src)
+                self.conditions[key] = rasterio.open(src)
+                
+        for key, src in conflicts.items():
+            if key in self.conflicts:
+                print('vector {} already loaded!'.format(key))
+            else:
+                self.conflicts[key] = gpd.read_file(src)
                 
         self.extent = self.extent()
     
@@ -24,20 +33,35 @@ class GISHandler:
         """Gets condition data for a specified geography location (lon/lat), stores it in the GeoDataFrame, and returns the row."""
         x, y = self.coordinate(x, y)
                                     
-        if not self.points.loc[(self.points.x==x) & (self.points.y==y)].empty:
-            print('point exists, returning original data')
-            return self.points.loc[(self.points.x==x) & (self.points.y==y)]
+        #if not self.points.loc[(self.points.x==x) & (self.points.y==y)].empty:
+        #    print('point exists, returning original data')
+        #    return self.points.loc[(self.points.x==x) & (self.points.y==y)]
         
-        conditions = {'x': x, 'y': y, 'valid': True, 'geometry': Point(x, y)}
+        point = Point(x, y)
+        conditions = {'x': x, 'y': y, 'ok-conditions': True, 'ok-scope': True, 'ok-conflicts': True, 'geometry': point}
         
-        for key, raster in self.rasters.items():
+        # check if point is offshore in desired scope
+        for polygon in self.scope['geometry']:
+            if point.intersects(polygon):
+                conditions['ok-scope'] = True
+            else:
+                conditions['ok-scope'] = False
+        
+        # check if point intersects with any of the conflicts loaded as vectors
+        for key, vector in self.conflicts.items():
+            for polygon in vector['geometry']:
+                if point.intersects(polygon):
+                    conditions['ok-conflicts'] = False
+        
+        # check if point exists for all condition datasets, and pull condition data for point
+        for key, raster in self.conditions.items():
             index = raster.index(x, y)
             try:
                  conditions[key] = raster.read(1)[index] # yes, by default this only reads the first band, but this is probably okay
             except IndexError as error:
                 print('failed to read {} raster: {}'.format(key, error))
-            if conditions[key] <= 0: # yes, this assumes that zero is not a valid value. this is true for our current rasters, but isn't neccsarily correct
-                conditions['valid'] = False
+            if conditions[key] <= -1000: # yes, this assumes that zero is not a valid value. this is true for our current rasters, but isn't necessarily correct
+                conditions['ok-conditions'] = False
         
         self.points = self.points.append(conditions, ignore_index=True)
         return self.points.iloc[-1:]
@@ -68,7 +92,7 @@ class GISHandler:
         """Calculates largest square extent that includes data from all loaded rasters."""
         
         extent = [-180, 180, -90, 90] # format: [W, E, S, N]
-        for src in self.rasters.values():
+        for src in self.conditions.values():
             extent = [max(extent[0], src.bounds[0]), min(extent[1], src.bounds[2]), max(extent[2], src.bounds[1]), min(extent[3], src.bounds[3])]
             
         return extent
