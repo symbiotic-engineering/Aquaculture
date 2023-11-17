4ddaa203f77c483a81bf6d7fb312f3a2451709e0
ahasankhani <ahasankhani2019@fau.edu>
2023-11-17 09:52:53 -0500

    updated gis data with Oceans

--- wpaf_opt.py
+++ wpaf_opt.py
@@ -1,5 +1,5 @@
-from gis.gis_handler import GISHandler
 from optimization import OpData
+from gis_handler import GISHandler
 import optimization
 from utilities import *
 import copy
@@ -18,30 +18,29 @@ all_vars_default = all_vars_default  #+ ['p_wave_vec']
 # with wave average data
 #all_vars_default = all_vars_default  + ['x_wave_ave']
 
-conditions = {'current [m/s]': 'gis/data/Surface Current (m-s).tif',
-              'oxygen [mg/l]': 'gis/data/Surface Oxygen (mg-l).tif',
-              'salinity [PSU]': 'gis/data/Surface Salinity (PSU).tif',
-              'temperature [°C]': 'gis/data/Surface Temperature (C).tif',
-              'period [s]': 'gis/data/Wave Energy Period (s).tif',
-              'height [m]': 'gis/data/Wave Significant Height (m).tif',
-              'bathymetry [m]': 'gis/data/Bathymetry (m).tif',
-              #'distance to shore [m]': 'gis/data/Distance to Shore (m).tif',
-              'distance to port [m]': 'gis/data/Distance to Port (m).tif'}
+conditions = {'current [m/s]': 'Input Data/gis data/Surface Currents m-s (NODP 2016).tif',
+              'oxygen [mg/l]': 'Input Data/gis data/Surface Oxygen mg-l (NCEI 2019).tif',
+              'salinity [PSU]': 'Input Data/gis data/Surface Salinity PSU (NCEI 2019).tif',
+              'temperature [C]': 'Input Data/gis data/Surface Temperature C (NODP 2016).tif',
+              'period [s]': 'Input Data/gis data/Wave Energy Period s (NREL 2011).tif',
+              'height [m]': 'Input Data/gis data/Significant Wave Height m (NREL 2011).tif',
+              'bathymetry [m]': 'Input Data/gis data/Bathymetry Downsampled m (NGDC 1990).tif',
+              'distance to port [m]': 'Input Data/gis data/Distance to Port m (OCM 2019).tif'}
 
 # high fishing is above average, very high is more than one standard deviation above average
-conflicts = {'very high fishing traffic': 'gis/data/Very High Fishing Vessel Traffic.geojson',
-#            'high fishing traffic': 'data/High Fishing Vessel Traffic.geojson',
-             'marine protected areas': 'gis/data/Marine Protected Areas.geojson',
-             'danger zones': 'gis/data/Danger Zones and Restricted Areas.geojson',
-             'submarine': 'gis/data/Submarine Transit Lanes.geojson',
-             'torpex': 'gis/data/Cape Cod TORPEX.geojson',
-             'block island': 'gis/data/Block Island Renewable Energy Zone.geojson',
-             'ma wind': 'gis/data/MA Wind Energy Areas.geojson',
-             'wind lease': 'gis/data/Potential Wind Lease Areas.geojson',
-             'wind planning': 'gis/data/Wind Planning Areas.geojson',
-             'shipping': 'gis/data/Shipping Lanes.geojson'}
+conflicts = {'very high fishing traffic': 'Input Data/gis data/Very High Fishing Vessel Traffic (NODP 2022).geojson',
+#            'high fishing traffic': 'Input Data/gis data/High Fishing Vessel Traffic (NODP 2022).geojson',
+             'marine protected areas': 'Input Data/gis data/Marine Protected Areas (NMPAC 2020).geojson',
+             'danger zones': 'Input Data/gis data/Danger Zones and Restricted Areas (OCM 2022).geojson',
+             'submarine': 'Input Data/gis data/Submarine Transit Lanes (NODP 2016).geojson',
+             'torpex': 'Input Data/gis data/Cape Cod TORPEX (NODP 2016).geojson',
+             'block island': 'Input Data/gis data/Block Island Renewable Energy Zone (NODP 2010).geojson',
+             'ma wind': 'Input Data/gis data/MA Wind Energy Areas (NODP 2015).geojson',
+             'wind lease': 'Input Data/gis data/Potential Wind Lease Areas (BOEM 2023).geojson',
+             'wind planning': 'Input Data/gis data/Wind Planning Areas (BOEM 2023).geojson',
+             'shipping': 'Input Data/gis data/Shipping Lanes (OCS 2015).geojson'}
 
-waters = "gis/data/Northeast State and Federal Waters.geojson"
+waters = 'Input Data/gis data/Northeast State and Federal Waters (OCM 2018).geojson'
 
 handler = GISHandler(conditions, conflicts, waters)
 
